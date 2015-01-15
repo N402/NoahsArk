9626d5bd9bf1209075fdb0682b27dbe1450424c9
shonenada <shonenada@gmail.com>
2015-01-16 02:07:41 +0800

    refactor qiniu

--- ark/master/views.py
+++ ark/master/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, jsonify
-from flask.ext.login import login_required
+from flask.ext.login import login_required, current_user
 
-from ark.utils.qiniu import gen_upload_token
+from ark.utils.qiniu import gen_upload_token, hash_save_key
 
 
 master_app = Blueprint('master', __name__)
@@ -15,5 +15,18 @@ def index():
 @master_app.route('/uptoken')
 @login_required
 def uptoken():
-    uptoken = gen_upload_token()
+    data = {
+        'saveKey': hash_save_key(current_user.id),
+    }
+    uptoken = gen_upload_token(data)
+    return jsonify(uptoken=uptoken)
+
+
+@master_app.route('/uptoken/avatar')
+@login_required
+def uptoken_avatar():
+    data = {
+        'saveKey': hash_save_key(current_user.id, 'avatar'),
+    }
+    uptoken = gen_upload_token(data)
     return jsonify(uptoken=uptoken)
--- ark/utils/qiniu.py
+++ ark/utils/qiniu.py
@@ -7,8 +7,6 @@ from hashlib import sha1, md5
 from base64 import urlsafe_b64encode
 from datetime import datetime, timedelta
 
-from flask.ext.login import current_user
-
 
 def hmac_sha1(data):
     access_secret = os.environ['ARK_QINIU_ACCESS_SECRET']
@@ -22,26 +20,24 @@ def calc_ts(s):
     return calendar.timegm(deadline.timetuple())
 
 
-def gen_key(uid=None, filename=None):
-    uid = str(uid or current_user.id)
-    hash_uid = md5(uid).hexdigest()
+def hash_save_key(uid=None, filename=None):
+    hash_uid = md5(str(uid)).hexdigest()
     key = filename.split('.', 1)[0] if filename else uuid4().hex
     return '%s/%s$(ext)' % (hash_uid, key)
 
 
-def gen_encoded_policy(uid=None, filename=None):
+def gen_encoded_policy(data):
     scope = os.environ['ARK_QINIU_BUCKET']
     deadline = calc_ts(60 * 10)
-    returnBody = '{"name": $(fname), "key": $(key)}'
-    data = {
+    policy  = {
         'scope': scope,
         'deadline': deadline,
-        'saveKey': gen_key(uid, filename),
         'mimeLimit': 'image/*',
         'fsizeLimit': 1024 * 1024 * 3,
-        'returnBody': returnBody,
+        'returnBody': '{"name": $(fname), "key": $(key)}',
     }
-    return urlsafe_b64encode(json.dumps(data))
+    policy.update(data)
+    return urlsafe_b64encode(json.dumps(policy))
 
 
 def gen_encoded_sign(policy):
@@ -50,8 +46,8 @@ def gen_encoded_sign(policy):
     return encoded_sign
 
 
-def gen_upload_token(uid=None, filename=None):
-    encoded_policy = gen_encoded_policy(uid, filename)
+def gen_upload_token(data):
+    encoded_policy = gen_encoded_policy(data)
     encoded_sign = gen_encoded_sign(encoded_policy)
     return "%s:%s:%s" % (os.environ['ARK_QINIU_ACCESS_KEY'],
                          encoded_sign,
