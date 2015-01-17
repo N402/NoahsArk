3f2778ebac1ecd2587d12ee2256db8068816d6c0
shonenada <shonenada@gmail.com>
2015-01-18 00:10:40 +0800

    refactor files

--- ark/goal/models.py
+++ ark/goal/models.py
@@ -19,10 +19,12 @@ class Goal(db.Model):
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(300)) 
-    image_url = db.Column(db.String(300))
+    image_file_id = db.Column(db.Integer, db.ForeignKey('goal_file.id'))
     created = db.Column(db.DateTime, default=datetime.utcnow)
     operate_at = db.Column(db.DateTime)
     state = db.Column(db.Enum(*(GOAL_STATES.keys())), default='ready')
+
+    image = db.relationship('GoalFile', uselist=False)
 
 
 class GoalActivity(db.Model):
@@ -31,9 +33,11 @@ class GoalActivity(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'))
+    image_file_id = db.Column(db.Integer, db.ForeignKey('goal_file.id'))
     content = db.Column(db.String(300))
-    image_url = db.Column(db.String(300))
     created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    image = db.relationship('GoalFile', uselist=False)
 
 
 class GoalLikeLog(db.Model):
@@ -45,3 +49,14 @@ class GoalLikeLog(db.Model):
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
     created = db.Column(db.DateTime, default=datetime.utcnow)
     is_deleted = db.Column(db.Boolean, default=False)
+
+
+class GoalFile(db.Model):
+
+    __tablename__ = 'goal_file'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    file_url = db.Column(db.String(500))
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
+    created = db.Column(db.DateTime, default=datetime.utcnow)
