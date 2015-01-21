06e52e628279347649cbae0b8656286abbd8052f
shonenada <shonenada@gmail.com>
2015-01-22 01:06:32 +0800

    add ranking model and template

--- ark/ranking/models.py
+++ ark/ranking/models.py
@@ -3,9 +3,9 @@ from datetime import datetime
 from ark.exts import db
 
 
-class RankingBan(db.Model):
+class AccountRankingBan(db.Model):
 
-    __tablename__ ='ranking_ban'
+    __tablename__ = 'account_ranking_ban'
 
     id = db.Column(db.Integer, primary_key=True)
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
@@ -14,5 +14,20 @@ class RankingBan(db.Model):
     is_deleted = db.Column(db.Boolean, default=False)
 
     account = db.relationship(
-        'Account', uselist=False, foreign_keys='RankingBan.account_id',
+        'Account', uselist=False, foreign_keys='AccountRankingBan.account_id',
         backref=db.backref('bans', uselist=True, lazy='dynamic'))
+
+
+class GoalRankingBan(db.Model):
+
+    __tablename__ = 'goal_ranking_ban'
+
+    id = db.Column(db.Integer, primary_key=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'))
+    operator_id = db.Column(db.Integer, db.ForeignKey('account.id'))
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    is_deleted = db.Column(db.Boolean, default=False)
+
+    goal = db.relationship(
+        'Goal', uselist=False,
+         backref=db.backref('bans', uselist=True, lazy='dynamic'))
