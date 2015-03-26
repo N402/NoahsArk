396d99b342665d72b58f11049d41812ab7121e9e
shonenada <shonenada@gmail.com>
2015-03-26 15:52:14 +0800

    Bump version: 0.5.2 → 0.5.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name='NoahsArk',
-    version='0.5.2',
+    version='0.5.3',
     author="shonenada",
     author_email="shonenada@gmail.com",
     url="https://github.com/N402/NoahsArk",
